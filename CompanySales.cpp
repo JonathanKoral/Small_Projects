f2af9714fa2b8fdd248a5f2179ad380901da4fb9
Jonathan Koral <114113945+JonathanKoral@users.noreply.github.com>
2024-03-06 17:41:18 -0500

    Add files via upload

---
+++ CompanySales.cpp
@@ -0,0 +1,61 @@
+//This program will read the quarterly sales for a given number of years and will print
+//the year and sales in table format.
+
+//Jonathan Koral
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const int MAXYEARS = 10;
+const int MAXCOLUMS = 5;
+
+typedef int SalesType[MAXYEARS][MAXCOLUMS];
+
+void getSales(SalesType, int&);
+void printSales(SalesType, int);
+void printTableHeading();
+
+int main()
+{
+    int yearsUsed;
+    SalesType sales;
+    
+    getSales(sales, yearsUsed);
+    printTableHeading();
+    printSales(sales, yearsUsed);
+    
+    return 0;
+}
+
+void printTableHeading()
+{
+    cout << setw(40) << "YEARLY QUARTERLY SALES" << endl << endl << endl;
+    cout << setw(10) << "Year" << setw(10) << "Quarter 1"
+         << setw(10) << "Quarter 2" << setw(10) << "Quarter 3"
+         << setw(10) << "Quarter 4" << endl;
+}
+
+void getSales (SalesType table, int& numberOfYears)
+{
+    cout << "Please enter the number of years (1-" << MAXYEARS << ")" << endl;
+    cin >> numberOfYears;
+
+    for (int years = 0; years < numberOfYears; years++) {
+        cout << "Please enter the YEARS and 4 Quarter of sales:\n";
+        for (int colums = 0; colums < MAXCOLUMS; colums++) {
+            cin >> table[years][colums];
+        } 
+    }
+    cout << endl;
+}
+
+void printSales(SalesType table, int numberOfYears)
+{
+    for (int years = 0; years < numberOfYears; years++) {
+        for (int colums = 0; colums < MAXCOLUMS; colums++) {
+            cout << setw(10) << table[years][colums];
+        }
+        cout << endl;
+    }
+}
